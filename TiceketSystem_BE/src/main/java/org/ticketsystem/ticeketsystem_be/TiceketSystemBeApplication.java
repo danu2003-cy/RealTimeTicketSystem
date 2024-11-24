d47b4ce14c141926da48fe30e453bab53ca91044
Peenaka-official <peenakaforofficial@gmail.com>
2024-11-24 11:48:33 +0530

    Added BE and CLI files

---
+++ TiceketSystem_BE/src/main/java/org/ticketsystem/ticeketsystem_be/TiceketSystemBeApplication.java
@@ -0,0 +1,13 @@
+package org.ticketsystem.ticeketsystem_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiceketSystemBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiceketSystemBeApplication.class, args);
+    }
+
+}
---
+++ TiceketSystem_BE/src/test/java/org/ticketsystem/ticeketsystem_be/TiceketSystemBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ticketsystem.ticeketsystem_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiceketSystemBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
